9f46dadd0ed68ecf863c2371b395ef9fef39ce4d
Brayan M. Salazar <brayanms.hpp@icloud.com>
2026-05-21 20:56:03 -0400

    Project files.

---
+++ ActividadUnidad2/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ActividadUnidad2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.emailBox = new System.Windows.Forms.TextBox();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.showPasswordCheckBox = new System.Windows.Forms.CheckBox();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.usernameBox = new System.Windows.Forms.TextBox();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.exitButton);
+            this.panel1.Controls.Add(this.loginButton);
+            this.panel1.Controls.Add(this.emailBox);
+            this.panel1.Controls.Add(this.emailLabel);
+            this.panel1.Controls.Add(this.showPasswordCheckBox);
+            this.panel1.Controls.Add(this.passwordBox);
+            this.panel1.Controls.Add(this.passwordLabel);
+            this.panel1.Controls.Add(this.usernameBox);
+            this.panel1.Controls.Add(this.usernameLabel);
+            this.panel1.Location = new System.Drawing.Point(158, 118);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(390, 144);
+            this.panel1.TabIndex = 7;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(178, 109);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 15;
+            this.exitButton.Text = "Salir";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.OnExitButtonClicked);
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(55, 109);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(102, 23);
+            this.loginButton.TabIndex = 14;
+            this.loginButton.Text = "Iniciar Sesión";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.ValidateFields);
+            // 
+            // emailBox
+            // 
+            this.emailBox.Location = new System.Drawing.Point(115, 46);
+            this.emailBox.Name = "emailBox";
+            this.emailBox.Size = new System.Drawing.Size(136, 20);
+            this.emailBox.TabIndex = 13;
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Location = new System.Drawing.Point(12, 49);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(97, 13);
+            this.emailLabel.TabIndex = 12;
+            this.emailLabel.Text = "Correo Electrónico:";
+            // 
+            // showPasswordCheckBox
+            // 
+            this.showPasswordCheckBox.AutoSize = true;
+            this.showPasswordCheckBox.Location = new System.Drawing.Point(267, 73);
+            this.showPasswordCheckBox.Name = "showPasswordCheckBox";
+            this.showPasswordCheckBox.Size = new System.Drawing.Size(119, 17);
+            this.showPasswordCheckBox.TabIndex = 11;
+            this.showPasswordCheckBox.Text = "Mostrar contrasenia";
+            this.showPasswordCheckBox.UseVisualStyleBackColor = true;
+            this.showPasswordCheckBox.CheckedChanged += new System.EventHandler(this.OnShowPassword);
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Location = new System.Drawing.Point(115, 73);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.PasswordChar = '*';
+            this.passwordBox.Size = new System.Drawing.Size(136, 20);
+            this.passwordBox.TabIndex = 10;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(43, 76);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(66, 13);
+            this.passwordLabel.TabIndex = 9;
+            this.passwordLabel.Text = "Contrasenia:";
+            // 
+            // usernameBox
+            // 
+            this.usernameBox.Location = new System.Drawing.Point(115, 14);
+            this.usernameBox.Name = "usernameBox";
+            this.usernameBox.Size = new System.Drawing.Size(138, 20);
+            this.usernameBox.TabIndex = 8;
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(8, 17);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(101, 13);
+            this.usernameLabel.TabIndex = 7;
+            this.usernameLabel.Text = "Nombre de Usuario:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 364);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Actividad de la Unidad 2";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox emailBox;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.CheckBox showPasswordCheckBox;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox usernameBox;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ ActividadUnidad2/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ActividadUnidad2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ValidateFields(object sender, EventArgs e)
+        {
+            Regex usernameRegex = new Regex("^[a-z]{4}.+", RegexOptions.IgnoreCase);
+            string username = usernameBox.Text;
+
+            if (!usernameRegex.IsMatch(username))
+            {
+                MessageBox.Show("El nombre de usuario indicado no es válido.", "Error");
+                usernameBox.Focus();
+                return;
+            }
+
+            Regex emailRegex = new Regex(
+                "^[a-z].+@(gmail|hotmail|icloud)\\.com",
+                RegexOptions.IgnoreCase
+            );
+
+            string email = emailBox.Text;
+            if (!emailRegex.IsMatch(email))
+            {
+                MessageBox.Show(
+                    "El correo electrónico indicado no es válido o no está soportado actualmente.",
+                    "Error"
+                );
+                return;
+            }
+
+            string password = passwordBox.Text;
+            if (password.Length < 8)
+            {
+                MessageBox.Show("La contrasenia es demasiado corta.", "Error");
+                return;
+            }
+
+            MessageBox.Show(
+                $"Has iniciado sesión satisfactoriamente. Bienvenido {username}!",
+                "Éxito"
+            );
+        }
+
+        private void OnShowPassword(object sender, EventArgs e)
+        {
+            char passwordChar;
+            if (showPasswordCheckBox.Checked)
+            {
+                passwordChar = '\0';
+            }
+            else
+            {
+                passwordChar = '*';
+            }
+
+            passwordBox.PasswordChar = passwordChar;
+        }
+
+        private void OnExitButtonClicked(object sender, EventArgs e)
+        {
+            DialogResult reply = MessageBox.Show(
+                "Estás seguro que deseas salir?",
+                "Confirmación",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+
+            if (reply == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
